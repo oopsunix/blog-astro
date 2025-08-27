a9e2b96401229f9a7078b8bd673af3f59b8dc7f2
oopsunix <133087009+oopsunix@users.noreply.github.com>
2025-08-27 12:25:10 +0800

    refactor: signature use png images

--- src/components/AnimatedSignature.tsx
+++ src/components/AnimatedSignature.tsx
@@ -1,12 +1,13 @@
-import Svg from '@/assets/signature.svg?raw'
+import signaturePng from '@/assets/signature.png'
 
 export function AnimatedSignature() {
   return (
-    <div
-      className="animated-signature"
-      dangerouslySetInnerHTML={{
-        __html: Svg,
-      }}
-    ></div>
+    <div className="animated-signature">
+      <img 
+        src={signaturePng.src} 
+        alt="Signature" 
+        className="signature-image"
+      />
+    </div>
   )
 }
